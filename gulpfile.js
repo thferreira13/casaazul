236b98ffa9e7c72ce55fef8ba29f7d5acd81c2cd
thferreira13 <pwpzinhu@gmail.com>
2019-11-06 18:30:08 -0300

    gulp teste

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var watch = require('gulp-watch');
 
 //task sass
-gulp.task('sass', function(){return sass('sass/*.sass').pipe(gulp.dest('css'))});
+gulp.task('sass', function(){return sass('sass/**/*.sass').pipe(gulp.dest('css'))});
 
-//task defaul
+//task watch
+gulp.task('watch', function(){gulp.watch('sass/**/*.sass', ['sass'])});
+
+//task default
 gulp.task('default', function(){});
