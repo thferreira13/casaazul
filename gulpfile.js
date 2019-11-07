145b6005daee0816416bc60ac6b43ece76d49b94
thferreira13 <pwpzinhu@gmail.com>
2019-11-06 21:30:39 -0300

    erro no gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,16 @@ var sass = require('gulp-ruby-sass');
 var watch = require('gulp-watch');
 
 //task sass
-gulp.task('sass', function(){return sass('sass/**/*.sass').pipe(gulp.dest('css'))});
+gulp.task('sass', function(){
+    return sass('sass/**/*.sass')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('css'));
+});
 
 //task watch
-gulp.task('watch', function(){gulp.watch('sass/**/*.sass', ['sass'])});
+gulp.task('watch', function(){
+    gulp.watch('sass/**/*.sass', ['sass'])
+});
 
 //task default
-gulp.task('default', function(){});
+gulp.task('default', ['sass', 'watch']);
